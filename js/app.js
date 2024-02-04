c45683a1dfd9da913eb5024968175808d94b9137
walkerbuddy01 <136956295+walkerbuddy01@users.noreply.github.com>
2024-02-04 17:00:04 +0530

    product section improved

--- js/app.js
+++ js/app.js
@@ -1,99 +1,86 @@
 const cart = document.querySelector(".cart");
-
-const cart_container = document.querySelector(".cart-container")
-
+const cart_container = document.querySelector(".cart-container");
 const cart_close_button = document.querySelector(".ri-close-line");
+const cart_open_button = document.querySelector(".cart-section");
 
-const cart_open_button = document.querySelector(".cart-section")
-
-
-cart_open_button.addEventListener('click',()=>{
+cart_open_button.addEventListener("click", () => {
   cart.style.display = "flex";
-  setTimeout(()=>{
-    cart_container.style.transform = 'translateX(0%)'
-  },100)
+  setTimeout(() => {
+    cart_container.style.transform = "translateX(0%)";
+  }, 100);
+});
 
-  
-  
-})
-
-
-cart_close_button.addEventListener('click',()=>{
-  cart_container.style.transform = 'translateX(100%)'
-  setTimeout(()=>{
+cart_close_button.addEventListener("click", () => {
+  cart_container.style.transform = "translateX(100%)";
+  setTimeout(() => {
     cart.style.display = "none";
-  },500)
-})
-
- 
-
-
-
-
-
-
-
-
-
-
-
+  }, 500);
+});
 
 // <div class="item-1 item">
-//    <div class="product-image">
-//     <img src="./media/hero section image.png" alt="product-image">
-//    </div>
-//      <h5 class="product-name">Nike shoes</h5>
-//     <h6 class="product-price">$100</h5>
-//  </div>
+//                         <div class="product-image">
+//                             <i class="ri-shopping-bag-line"> Buy</i>
+//                          <img src="./media/hero section image.png" alt="product-image">
+//                         </div>
+//                           <h5 class="product-name">Nike shoes</h5>
+//                          <h6 class="product-price">$100</h5>
+//                       </div>
 
 const top_Product_Shelf = document.querySelector(".item-section-top");
 const bottom_Product_Shelf = document.querySelector(".item-section-bottom");
 const loader = document.createElement("div");
-    loader.setAttribute("class","loader");
+loader.setAttribute("class", "loader");
 
-
-
-    // GET product 
+// GET product
 
 async function getProducts(parentElement) {
-    parentElement.appendChild(loader);
-  const response = await fetch(
-    "https://fakestoreapi.com/products?limit=5"
-  );
+  parentElement.appendChild(loader);
+  const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
   return data;
 }
 
-// Adding Items 
+// Adding Items
 
 async function addingItems(parentElement) {
-    
-    const items = await getProducts(parentElement);
-    setTimeout(()=>{
-        
-        for (let item of items) {
-            const title = item.title;
-            const price = item.price;
-            const image = item.image;
-            
-            const DomElement = document.createElement("div");
-            DomElement.setAttribute("class","item-1 item")
-            
-            DomElement.innerHTML = `
+  const items = await getProducts(parentElement);
+  setTimeout(() => {
+    for (let item of items) {
+      const title = item.title;
+      const price = item.price;
+      const image = item.image;
+
+      const DomElement = document.createElement("div");
+      DomElement.setAttribute("class", "item-1 item");
+
+      DomElement.innerHTML = `
             <div class="product-image">
+            <i class="ri-shopping-bag-line">Buy</i>
             <img src="${image}" alt="product-image" loading = "lazy">
             </div>
             <h5 class="product-name">${title}</h5>
             <h6 class="product-price">$${price}</h5>
-            `
-            loader.style.display = "none";
-            parentElement.appendChild(DomElement);
-        }  
-},1200)
+            `;
+      loader.style.display = "none";
+      parentElement.appendChild(DomElement);
+    }
+  }, 1200);
+}
 
-  }
+addingItems(bottom_Product_Shelf);
+addingItems(top_Product_Shelf);
+
+ function select() {
+  const item = document.querySelector(".item");
+  console.log(item  );
+  const buybutton =  item.querySelector(".ri-shopping-bag-line");
+  buybutton.addEventListener("click", (e) => {
+    console.log(e);
+  })
+};
+
+setTimeout(select,1500)
 
 
-  addingItems(bottom_Product_Shelf);
-  addingItems(top_Product_Shelf);
+
 
