e63c410971a03d17bf630f9dc7da947509d236a6
walkerbuddy01 <136956295+walkerbuddy01@users.noreply.github.com>
2024-02-05 23:05:22 +0530

    Cart functionality Unable + minor UI changes

--- js/app.js
+++ js/app.js
@@ -17,19 +17,36 @@ cart_close_button.addEventListener("click", () => {
   }, 500);
 });
 
-// <div class="item-1 item">
-//                         <div class="product-image">
-//                             <i class="ri-shopping-bag-line"> Buy</i>
-//                          <img src="./media/hero section image.png" alt="product-image">
-//                         </div>
-//                           <h5 class="product-name">Nike shoes</h5>
-//                          <h6 class="product-price">$100</h5>
-//                       </div>
-
+{/* <div class="item-1 item">
+                        <div class="product-image">
+                            <i class="ri-shopping-bag-line"> Buy</i>
+                         <img src="./media/hero section image.png" alt="product-image">
+                        </div>
+                          <h5 class="product-name">Nike shoes</h5>
+                         <h6 class="product-price">$100</h5>
+                      </div> */}
+ 
 const top_Product_Shelf = document.querySelector(".item-section-top");
-const bottom_Product_Shelf = document.querySelector(".item-section-bottom");
+const cart_product_section = document.querySelector(".products-collection")
 const loader = document.createElement("div");
 loader.setAttribute("class", "loader");
+// Add to cart function 
+const add_in_cart = (item)=>{
+  const product = document.createElement("div");
+  product.setAttribute("class", "product");
+  product.innerHTML=`<div class="cart-product-image">
+  <img src="${item.image}" alt="">
+</div>
+<div class="cart-product-content">
+  <h5 class="product-name">${item.title}</h5>
+  <h5 class="product-price">$${item.price}</h5>
+  <div class="counter">
+      
+  </div>
+</div>`
+cart_product_section.appendChild(product);
+}
+
 
 // GET product
 
@@ -37,6 +54,7 @@ async function getProducts(parentElement) {
   parentElement.appendChild(loader);
   const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -49,35 +67,38 @@ async function addingItems(parentElement) {
       const title = item.title;
       const price = item.price;
       const image = item.image;
-
+      const desc = item.description;
+      const Category = item.category;
+      
       const DomElement = document.createElement("div");
       DomElement.setAttribute("class", "item-1 item");
 
       DomElement.innerHTML = `
             <div class="product-image">
-            <i class="ri-shopping-bag-line">Buy</i>
-            <img src="${image}" alt="product-image" loading = "lazy">
-            </div>
-            <h5 class="product-name">${title}</h5>
-            <h6 class="product-price">$${price}</h5>
+                         <img src="${image}" alt="product-image">
+                        </div>
+                         <div class="product-content">
+                            <h4>${title}</h4>
+                            <p class="product-desc">
+                            ${desc}
+                           </p>
+                            <h4>Category:${Category}</h4>
+                            <h4>Price:$ ${price}</h4>
+                            <button  class="add_to_cart">Add to cart</button>
+                         </div>
             `;
       loader.style.display = "none";
       parentElement.appendChild(DomElement);
+      DomElement.querySelector(".add_to_cart").addEventListener("click",()=>{
+        add_in_cart(item);
+      })
     }
   }, 1200);
 }
 
-addingItems(bottom_Product_Shelf);
 addingItems(top_Product_Shelf);
 
- function select() {
-  const item = document.querySelector(".item");
-  console.log(item  );
-  const buybutton =  item.querySelector(".ri-shopping-bag-line");
-  buybutton.addEventListener("click", (e) => {
-    console.log(e);
-  })
-};
+ 
 
 setTimeout(select,1500)
 
